660cdb723f6b142d51edfadf57cb016d79950f4a
Fabbe88 <primoviking88@gmail.com>
2020-08-05 16:39:02 +0200

    Update cms.js

--- src/js/cms.js
+++ src/js/cms.js
@@ -6,14 +6,16 @@ import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../c
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
-import ProductsPreview from "./cms-preview-templates/products";
-import ValuesPreview from "./cms-preview-templates/values";
+import ProductsPreview from "./cms-preview-templates/data";
+import ValuesPreview from "./cms-preview-templates/software";
 import ContactPreview from "./cms-preview-templates/contact";
+import ContactPreview from "./cms-preview-templates/test";
 
 CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
-CMS.registerPreviewTemplate("products", ProductsPreview);
-CMS.registerPreviewTemplate("values", ValuesPreview);
+CMS.registerPreviewTemplate("data", DataPreview);
+CMS.registerPreviewTemplate("software", SoftwarePreview);
 CMS.registerPreviewTemplate("contact", ContactPreview);
+CMS.registerPreviewTemplate("test", TestPreview);
 CMS.init();
